8bf0568d6553e01fad8028bf9fbc9a101c949e41
George Panaretou <gpanaretou@hotmail.com>
2025-01-16 17:50:30 +0200

    added sqlc to allow for sql to go code with type safety, added register function

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/gpanaretou/Gator/internal/config"
+	"github.com/gpanaretou/Gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -24,6 +31,8 @@ type commands struct {
 func (c *commands) register(name string, f func(*state, command) error) {
 	switch name {
 	case "login":
+		c.available[name] = f
+	case "register":
 		c.available[name] = f
 	}
 }
@@ -41,6 +50,11 @@ func (c *commands) run(s *state, cmd command) error {
 		if err != nil {
 			return err
 		}
+	case "register":
+		err := c.available[cmd.name](s, cmd)
+		if err != nil {
+			return err
+		}
 	default:
 		err := fmt.Errorf("-- '%v' command not implemented", cmd.name)
 		return err
@@ -51,17 +65,47 @@ func (c *commands) run(s *state, cmd command) error {
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
-		err := fmt.Errorf("login requires exactly 1 arguement")
+		err := fmt.Errorf("login requires exactly 1 arguement: login <name>")
 		return err
 	}
 
 	user_name := cmd.args[0]
-	err := s.cfg.SetUser(user_name)
+
+	_, err := s.db.GetUser(context.Background(), user_name)
+	if err != nil {
+		return fmt.Errorf("%v user does not exist", user_name)
+	}
+
+	err = s.cfg.SetUser(user_name)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("> User is set to: %v", user_name)
+	fmt.Printf("> User is set to: %v\n", user_name)
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("register requires 1 arguement: register <name>")
+	}
+
+	user_name := cmd.args[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      user_name,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		return fmt.Errorf("user with name %v already exists", user_name)
+	}
+
+	s.cfg.SetUser(user_name)
+	fmt.Printf("*SYSTEM: USER %v was created successfully\n", user_name)
+	fmt.Printf("*SYSTEM: USER: \n%v\n", user)
 
 	return nil
 }
@@ -76,7 +120,16 @@ func main() {
 	}
 	cmds.available["login"] = nil
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 	args := os.Args
+
+	db, err := sql.Open("postgres", s.cfg.DbURL)
+	if err != nil {
+		log.Fatal("error while trying to connect to the Database", err)
+	}
+
+	dbQueries := database.New(db)
+	s.db = dbQueries
 
 	if len(args) < 2 {
 		log.Fatal("was expecting at least one arguement")
@@ -86,7 +139,7 @@ func main() {
 	cmd.name = args[1]
 	cmd.args = args[2:]
 
-	err := cmds.run(&s, cmd)
+	err = cmds.run(&s, cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
